6601532090e357e7e516fe76f88d82c27a0dbf54
Yas <msb5003@outlook.jp>
2016-11-30 21:33:54 +0900

    modify the environment variables

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ excluded_dirs() {
   for i in $(find . -maxdepth 1 -mindepth 1 -not -path *./.* -type d)
     do pushd $i > /dev/null &&
 
-      export $1=${i#*/}
+      local $1=${i#*/}
       [ -z $3 ] &&
 
-        export imagename="${IMAGENAME_PREFIX}${name#*-}:${tag#*-}"
+        local imagename="${IMAGENAME_PREFIX}${name#*-}:${tag#*-}" &&
         docker build ${build_opts} --tag ${imagename} . &&
           docker push ${imagename}
 
